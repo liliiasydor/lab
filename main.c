fc4e1469a81303a2be83e56740c8c0c7adc42b08
liliiasydor <liliia.sydor.pz.2025@lpnu.ua>
2026-02-16 10:11:28 +0200

    add

--- main.c
+++ main.c
@@ -1,26 +1,17 @@
-#include <windows.h>
 #include "Functions.h"
-
 int main(void)
 {
-    SetConsoleCP(1251);       
-    SetConsoleOutputCP(1251);
-    Node* head = NULL;
-    Node* tail = NULL;
-
-    if (!list_read_from_file("DataList.txt", &head, &tail)) {
-        printf("Помилка відкриття файлу\n");
-        return 1;
-    }
-
-    list_print(head);
-    printf("\n--- Sorted by age (desc) ---\n");
-    list_sort_by_age_desc(&head, &tail);
-    list_print(head);
-
-    printf("\n--- Only students with grade 2 ---\n");
-    list_remove_without_grade2(&head, &tail);
-    list_print(head);
-
-    return 0;
+	system("chcp 1251");
+	
+	Node* head=NULL;
+	Node* tail = NULL;
+	read_from_file_list("DataList.txt", &head, &tail);
+	print_list(head, tail);
+	sort_list(&head, &tail);
+	printf("\n");
+	print_list(head, tail);
+	printf("\n");
+	remove_bad(&head, &tail);
+	print_list(head, tail);
+	return 0;
 }
